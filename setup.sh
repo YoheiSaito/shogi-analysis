cb66ae3c8d1678bff741b5db06a88c6b33a080c9
YoheiSaito <teyo.13324@gmail.com>
2020-11-29 18:43:22 +0900

    update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 git clone https://github.com/yaneurao/YaneuraOu.git
 git clone https://github.com/yaneurao/Ayane.git
 
-cd YaneuraOu/source && make -j4
-cd ..
+cd YaneuraOu/source && make -j4 && cd ../../
 ln -s YaneuraOu/source/YaneuraOu-by-gcc .
 ln -s Ayane/source/shogi .
